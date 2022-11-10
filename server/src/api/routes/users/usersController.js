abd6c7175fd227e6db7e7c39186f653e33f27449
Morgen Diaz <morgen@webslinger.works>
2022-11-10 08:26:07 -1000

    Users login endpoint responds with generated web token.

--- server/src/api/routes/users/usersController.js
+++ server/src/api/routes/users/usersController.js
@@ -1,29 +1,41 @@
-const { hashPassword, passwordsDoMatch } = require('../../../../security');
-const usersModel = require('../../../db/models/user');
-const { UserDoesNotExistError, UnexpectedServerError, PasswordDoesNotMatch } = require('../../errors');
+const jwt = require("jsonwebtoken");
+const { passwordsDoMatch } = require("../../../../security");
+const usersModel = require("../../../db/models/user");
+const {
+  UserDoesNotExistError,
+  UnexpectedServerError,
+  PasswordDoesNotMatch,
+} = require("../../errors");
 
-async function login(req, res, next){
-    try {
-        const {email, password} = req.body; 
-        const user = await usersModel.getByEmail(email);
-        if(!user) {
-            return next(UserDoesNotExistError(email));
-        }
+const { JWT_SECRET } = process.env;
 
-        const isMatchingPassword = await passwordsDoMatch(password, user.password);
-        
-        if (!isMatchingPassword) {
-            return next(PasswordDoesNotMatch());
-        }
-        
-        res.json(user);
+async function login(req, res, next) {
+  try {
+    const { email, password } = req.body;
 
-    } catch (error) {
-        console.error(error);
-        return next(UnexpectedServerError())
+    const user = await usersModel.getByEmail(email);
+    if (!user) {
+      return next(UserDoesNotExistError(email));
     }
+
+    const isMatchingPassword = await passwordsDoMatch(password, user.password);
+
+    if (!isMatchingPassword) {
+      return next(PasswordDoesNotMatch());
+    }
+
+    const token = jwt.sign(
+      { id: user.id, name: user.name, userRole: user.userRole },
+      JWT_SECRET
+    );
+
+    res.json({ token, user });
+  } catch (error) {
+    console.error(error);
+    return next(UnexpectedServerError());
+  }
 }
 
 module.exports = {
-    login
-}
+  login,
+};
