403f0a09eeae1e9a559f04a59dd40515bfb78804
Kas Boyd <git.kasb0yd@gmail.com>
2022-11-17 19:04:24 -0800

    added sample product data and images.

---
+++ server/src/data/productData.js
@@ -0,0 +1,20 @@
+const products = [
+    {"product_name": "Test Product1", "price":"$30.00", "inventory_quanitity": "3", "description": "This is a test product", "brand":"Test Brand", "image_url":"" },
+    {"product_name": "Magma Red CUH-ZCT2U DualShock 4 Wireless Controller", "price":"$40.00", "inventory_quanitity": "3", "description": "Wireless Controller for PlayStation 4 - Magma Red", "brand":"Sony", "image_url":"" },
+    {"product_name": "Jet Black CUH-ZCT2 DualShock 4 Wireless Controller", "price":"$35.00", "inventory_quanitity": "3", "description": "Wireless Controller for PlayStation 4 - Jet Black", "brand":"Sony", "image_url":"" },
+    {"product_name": "Electric Volt QAU-00021 Wireless Controller", "price":"$50.00", "inventory_quanitity": "3", "description": "Xbox Series X|S Wireless Controller Electric Volt", "brand":"Microsoft", "image_url":"" },
+    {"product_name": "Black QAU-00022 Wireless Controller", "price":"$35.00", "inventory_quanitity": "3", "description": "Xbox Series Wireless Controller Black", "brand":"Microsoft", "image_url":"" },
+    {"product_name": "Princess Peach and Princess Daisy Deluxe Switch Travel Case", "price":"$35.00", "inventory_quanitity": "3", "description": "Nintendo 232208 Super Mario Bros Themed Case for the Switch", "brand":"Nintendo", "image_url":"" },
+    {"product_name": "Mario Kart Racing Wheel", "price":"$38.00", "inventory_quanitity": "3", "description": "Refurbished Hori Mario Kart Racing Wheel Pro Mini for Nintendo Switch", "brand":"Nintendo", "image_url":"" },
+    {"product_name": "Playstation Camera", "price":"$43.00", "inventory_quanitity": "3", "description": "Refurbished PlayStation 3005726 PS5 HD Camera, Dual Lense 1080p", "brand":"Playstation", "image_url":"" },
+    {"product_name": "Foldable Video Drone", "price":"$100.00", "inventory_quanitity": "3", "description": "Refurbished Vivitar DRC447 SkyHawk Foldable Video Drone, 1080P HD Live Video RC Quadcopter", "brand":"Vivitar", "image_url":"" },
+    {"product_name": "XBox Game Drive", "price":"$70.00", "inventory_quanitity": "3", "description": "Refurbished Seagate STKX2000403 Game Drive for Xbox 2TB External USB 3.2 Gen 1 Hard Drive, Grey", "brand":"Seagate", "image_url":"" },
+    {"product_name": "Pokemon Ultraball Wireless Controller Nintendo Switch", "price":"$30.00", "inventory_quanitity": "3", "description": "Refurbished PowerA 1511636-01 Enhanced Wireless Controller for Nintendo Switch Pokemon Ultra Ball", "brand":"PowerA", "image_url":"" },
+    {"product_name": "Lite Hybrid System Armor Nintendo Switch", "price":"$8.00", "inventory_quanitity": "3", "description": "Refurbished Hori NS2-054U Nintendo Switch Lite Hybrid System Armor (Yellow)", "brand":"Hori", "image_url":"" },
+    {"product_name": "Mario Wireless Controller Nintendo Switch", "price":"$31.00", "inventory_quanitity": "3", "description": "Referbished Hori Nintendo Switch Gaming Controller", "brand":"Hori", "image_url":"" },
+    {"product_name": "Wolverine Ultimate Chroma Controller", "price":"$112.00", "inventory_quanitity": "3", "description": "Multi-Purpose Refurbished Razer RZ06-02250100-R3U1 Wolverine Ultimate Chroma Controller - Xbox One, PC", "brand":"Razer", "image_url":"" },
+]
+
+module.exports = {
+    products
+}
