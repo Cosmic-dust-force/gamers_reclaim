e1936533718ffbdbe3a46309ac65cfc67912aba8
Morgen Diaz <morgen@webslinger.works>
2022-11-26 16:24:50 -1000

    Api returns error if user tries to checkout with no items in cart.

--- server/src/api/routes/orders/ordersController.js
+++ server/src/api/routes/orders/ordersController.js
@@ -1,10 +1,20 @@
+const cartItemsModel = require("../../../db/models/cartItem");
 const ordersModel = require("../../../db/models/order");
-const { UnexpectedServerError } = require("../../errors");
+const {
+  UnexpectedServerError,
+  OrderRequiresItemsInCartError,
+} = require("../../errors");
 
 const moment = require("moment");
 
 async function checkoutUserCart(req, res, next) {
   try {
+    const itemsInCart = await cartItemsModel.itemsInCartForUser(req.user.id);
+
+    if (!itemsInCart.length) {
+      return next(OrderRequiresItemsInCartError());
+    }
+
     const dateNow = moment().format("YYYY-MM-DD HH:mm:ss");
     const newOrder = await ordersModel.checkout(req.user.id, dateNow);
 
