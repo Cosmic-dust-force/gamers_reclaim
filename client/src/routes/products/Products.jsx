c51a85824e83db613674a823cdfdfbf08d724de3
Kas Boyd <git.kasb0yd@gmail.com>
2022-12-04 09:44:53 -0800

    resolved conflict with products file for cart-ui merge.

--- client/src/routes/products/Products.jsx
+++ client/src/routes/products/Products.jsx
@@ -6,7 +6,11 @@ export default function Products() {
 
   return (
     <div>
-      <ProductsList products={products} />
+        <h1 className="text-center font-semibold text-5xl uppercase tracking-wide mt-6">
+            PRODUCTS
+        </h1>
+        <div className="font-bebas text-center font-semibold text-3xl uppercase tracking-wide m-3"></div>
+        <ProductsList products={products} />
     </div>
-  );
+)
 }
