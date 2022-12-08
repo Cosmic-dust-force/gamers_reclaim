aa7fccc5e0585be040f3a005952ec9e558cc3823
tylermcmu <94275794+tylermcmu@users.noreply.github.com>
2022-12-08 10:45:49 -0800

    Increased spacing between elements to avoid overlap with footer.

--- client/src/routes/checkout/Cart.jsx
+++ client/src/routes/checkout/Cart.jsx
@@ -8,7 +8,6 @@ import CartItem from "./CartItem";
 import CheckoutOptions from "./CheckoutOptions";
 const emptyCartImage = require("../../img/empty_cart.png");
 
-
 export default function Cart() {
   const navigate = useNavigate();
   const { cartItems, removeItemFromCart, updateItemQuantity, checkout } =
@@ -49,24 +48,21 @@ export default function Cart() {
   const subtotal = getSubtotal();
 
   return (
-    <main className="grow flex flex-col flex-wrap">
+    <main className="grow flex flex-col flex-wrap mb-6">
       <h1 className="text-center font-semibold text-5xl uppercase tracking-wide mt-6 mb-6">
         Shopping Cart
       </h1>
       <div className="flex flex-wrap bg-white text-center mt-6 justify-center">
-        {!cartItems.length &&
+        {!cartItems.length && (
           <div>
             <p>You don't have any items in your cart, let's get gaming!</p>
-            <img
-              src={emptyCartImage}
-              alt="handheld videogame console"
-            />
+            <img src={emptyCartImage} alt="handheld videogame console" />
             <LinkButton
               value={"View Our Products"}
               clickHandler={handleNavigateToProducts}
             />
           </div>
-        }
+        )}
         {cartItems.map((cartItem) => (
           <CartItem
             key={cartItem.id || "tmp-cart-item-key" + cartItem.productId}
@@ -84,11 +80,13 @@ export default function Cart() {
         <div className="flex flex-col items-stretch justify-center flex-wrap">
           <h4 className="m-4 text-center text-2xl mb-6 ">{`Subtotal: $${subtotal}`}</h4>
           <div className="p-3">
-            <CheckoutOptions user={user} proccesOrderHandler={handleProcessOrder} />
+            <CheckoutOptions
+              user={user}
+              proccesOrderHandler={handleProcessOrder}
+            />
           </div>
         </div>
-      ) :
-        null}
+      ) : null}
     </main>
   );
 }
